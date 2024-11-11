edfc6d51bc7fbc5c1a384e73903a539b0b38a0ec
Ifrazahoor <ifra8766@gmail.com>
2024-11-11 05:13:44 +0500

    portfolio

--- 14.2.2/src/app/about/page.tsx
+++ 14.2.2/src/app/about/page.tsx
@@ -4,14 +4,7 @@ import { FaCss3Alt } from "react-icons/fa";
 import { TbBrandJavascript } from "react-icons/tb";
 import { FaPython } from "react-icons/fa6";
 import { FaReact } from "react-icons/fa";
-
-
-
-
 import Image from "next/image";
-
-
-
 export default function About(){
     return(
         <div className="about">
@@ -23,15 +16,14 @@ export default function About(){
                     <h1>ABOUT</h1>
                 </div>
                 <div className="h3head">
-                    <h3>me Professional Web Designer And & Developer</h3>
+                    <h3>me Professional Web Designer And Developer</h3>
                 </div>
                 <div className="paraabout">
-                    <p>me Ifra Zahoor a professional UI/UX designer with a passion for creating intuitive and engaging digital experiences. I specialize in crafting user interfaces that are both visually appealing and highly functional, ensuring a seamless interaction between users and the product. My approach is driven by a deep understanding of user behavior, focusing on making interfaces intuitive, easy to navigate, and aesthetically aligned with the brand. By conducting thorough user research, wireframing, and prototyping, I ensure that every design decision enhances usability and meets user needs. My goal is to create designs that not only look great but also deliver a superior user experience, making every interaction meaningful.
-
-</p>
+                    <p>me Ifra Zahoor a professional UI/UX designer with a passion for creating intuitive and engaging digital experiences.I specialize in crafting user interfaces that are both visually appealing and highly functional ensuring a seamless interaction between users and the product.My approach is driven by a deep understanding of user behavior focusing on making interfaces intuitive easy to navigate and aesthetically aligned with the brand.By conducting thorough user research wireframing and prototyping I ensure that every design decision enhances usability and meets user needs My goal is to create designs that not only look great but also deliver a superior user experienc  making every interaction meaningful.
+              </p>
                 </div>
                 <div className="para2">
-                    <p>me a professional web designer and developer with a passion for creating visually appealing and highly functional websites. I specialize in using modern web technologies like HTML, CSS, and JavaScript to design responsive, user-friendly interfaces. My approach focuses on blending aesthetics with performance to deliver seamless digital experiences. Whether it's building from scratch or revamping an existing site, I ensure each project is tailored to my client's unique needs. My goal is to craft websites that not only look great but also provide excellent usability and engagement.</p>
+                    <p>me a professional web designer and developer with a passion for creating visually appealing and highly functional websites.I specialize in using modern web technologies like HTML CSS and JavaScript to design responsive user friendly interfaces.My approach focuses on blending aesthetics with performance to deliver seamless digital experiences. Whether its building from scratch or revamping an existing site I ensure each project is tailored to my clients unique needs.My goal is to craft websites that not only look great but also provide excellent usability and engagement</p>
                 </div>
                 <div className="btnabout">
                 <button className="btnabout1">Download CV</button>
@@ -39,20 +31,18 @@ export default function About(){
                 <div className="skills">
                     <h1 className="sub-title">My<span>Skills</span></h1>
                     <section>
-                        <div className="container1" id="skills">
+                        <div className="container1"id="skills">
                             <h1 className="heading">Technical Skills</h1>
                             <div className="technical-bars">
                                 <div className="bar"><FaHtml5 />
                                 <div className="indfo">
                                     <span>HTML</span>
                                     </div>
-                            
-                            <div className="progress-line html">
+                              <div className="progress-line html">
                                 <span></span>
                                 </div>
                             </div>
-                            
-                                <div className="bar"><FaCss3Alt />
+                             <div className="bar"><FaCss3Alt />
                                  <div className="indfo">
                                     <span>CSS</span>
                             </div>
@@ -86,8 +76,7 @@ export default function About(){
                             </div>
                         </div>
                         </div>
-                        
-                    </section>
+                        </section>
                 </div>
             </div>
         </div>
